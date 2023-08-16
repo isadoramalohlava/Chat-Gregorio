d311529266e6dc81cd2212dadf56efebe811fd92
gregoriolombardi <gregorio.lombardi@gmail.com>
2023-08-16 13:42:48 -0300

    alteraçoes

--- apiConfig.js
+++ apiConfig.js
@@ -1,3 +1,7 @@
-const API_BASE_URL = 'https://duble.preddita.com'; // Altere para o novo endereço
+// const API_BASE_URL = 'https://duble.preddita.com'; // Altere para o novo endereço
+
+// export default API_BASE_URL;
+
+const API_BASE_URL = 'http://localhost:8000'; // Altere para o novo endereço
 
 export default API_BASE_URL;
